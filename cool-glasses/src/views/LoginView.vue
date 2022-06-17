d884bfecc59dcb2f8b906fe62cea57682e98c0e3
RossiEduardo <eduardorossi80@hotmail.com>
2022-06-17 16:00:53 -0300

    Adicionando verificacao dos campos nas paginas de login e de cadastro

--- cool-glasses/src/views/LoginView.vue
+++ cool-glasses/src/views/LoginView.vue
@@ -11,5 +11,34 @@ export default {
   components: {
     Login
   },
+  mounted(){
+    const submit = document.getElementById("botao_entrar");
+
+// submit.addEventListener("click", validaVazio);
+submit.addEventListener("click", validaEntradas);
+
+
+const nome = document.getElementById("nome");
+const senha = document.getElementById("password");
+
+
+function validaEntradas(e) {
+    e.preventDefault();
+    let valid = true;
+    var re_email = /\S+@\S+\.\S+/;
+
+    if (!(re_email.test(email.value))){
+        if (!email.value)
+            alert("Insira seu email");
+        else
+            alert("Insira um email valido");
+    }
+
+    if (!senha.value)
+        alert("Insira sua senha");
+
+    return valid;
+  }
+  }
 }
 </script>
