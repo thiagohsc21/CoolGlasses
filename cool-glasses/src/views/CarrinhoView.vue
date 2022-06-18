e268bfe936fd3b473096b3df5ddb9f866dc3c875
Ádrio <adrio20@usp.br>
2022-06-18 19:18:19 -0300

    Solução para o fetch no carrinho
    tem que por tudo dentro do fetch pra dar certo

--- cool-glasses/src/views/CarrinhoView.vue
+++ cool-glasses/src/views/CarrinhoView.vue
@@ -8,28 +8,6 @@
 import Carrinho from '@/components/Carrinho.vue'
 import compras from '@/objects/carrinho.js'
 
-compras = compras.compras;
-
-var produtos = null;
-
-fetch('http://localhost:3000/produtos')
-  .then(res => res.json())
-  .then(data => {
-    produtos = data;
-  })
-  .catch(err => alert(err.message));
-
-console.log('produtos depois: ', produtos);
-
-let produtoNoCarrinho = [];
-
-compras.forEach((compra, idx) => {
-  produtoNoCarrinho.push(produtos[compra.id])
-  produtoNoCarrinho[idx].qtd = compra.qtd
-});
-
-this.c = produtoNoCarrinho;
-
 export default {
   name: 'CarrinhoView',
   data () {
@@ -42,6 +20,24 @@ export default {
     Carrinho
   },
   mounted() {
+    compras = compras.compras;
+
+    fetch('http://localhost:3000/produtos')
+      .then(res => res.json())
+      .then(data => {
+        let produtos = data;
+        
+        let produtoNoCarrinho = [];
+
+        compras.forEach((compra, idx) => {
+          produtoNoCarrinho.push(produtos[compra.id]);
+          produtoNoCarrinho[idx].qtd = compra.qtd;
+        });       
+
+        this.c = produtoNoCarrinho;
+      })
+      .catch(err => alert(err.message));
+
   }
 }
 </script>
